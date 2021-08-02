222481ebf227fe8242bc04a3a0e503867757f003
Charles <chall70101@gmail.com>
2021-08-02 17:58:23 -0600

    commit tree 377392594d07b53aa67d8dae22bfc23985eeada9

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // Write your code here
+const main = document.getElementById('main')
+
 main.remove();
 
 const newHeader = document.createElement('h1')
-document.body.appendChild(newHeader)
+
+
 newHeader.id = 'victory'
-victory.innerHTML = "Charlie is the champion"
+
+
+newHeader.innerHTML = 'Charlie is the champion'
+
+document.body.appendChild(newHeader)
